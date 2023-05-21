d224afea8935c39fa9db9e30699674af0e2403c7
felippodev <coelho.luizfelippo@gmail.com>
2023-05-21 11:40:50 -0300

    added new method StandardRetentionPlanTests.test_retention_days_check and altered logic in the test_retain_snapshot

--- test_retention_plan.py
+++ test_retention_plan.py
@@ -11,19 +11,11 @@ class StandardRetentionPlanTests:
     
     def test_retain_snapshot(self):
         today = date.today()
-        retention_time = today - timedelta(days=42)
+        invalid_date = today - timedelta(days=80)
         
-        assert self.retention_plan.retain_snapshot(retention_time) == True
         assert self.retention_plan.retain_snapshot(today) == True
-        assert (
-            self.retention_plan.retain_snapshot(today + timedelta(days=1)) \
-                == False
-            )
-        assert (
-            self.retention_plan.retain_snapshot(today - timedelta(days=43)) \
-                == False
-            )
-        
+        assert self.retention_plan.retain_snapshot(invalid_date) == False
+                                                
     def test_input_validation(self):
         assert self.retention_plan.input_validation("2023-05-21") == False
         assert self.retention_plan.input_validation(datetime.now()) == False
@@ -38,6 +30,21 @@ class StandardRetentionPlanTests:
                 + timedelta(days=10)) == True
             )
 
+    def test_retention_days_check(self):
+        today = date.today()
+        retention_time = today - timedelta(days=42)
+        
+        assert self.retention_plan.retain_snapshot(retention_time) == True
+        assert self.retention_plan.retain_snapshot(today) == True
+        assert (
+            self.retention_plan.retain_snapshot(today + timedelta(days=1)) \
+                == False
+            )
+        assert (
+            self.retention_plan.retain_snapshot(today - timedelta(days=43)) \
+                == False
+            )
+        
 
 @mark.gold
 class GoldRetentionPlanTests:
